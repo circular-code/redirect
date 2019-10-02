a435b9cce2b6b77277ebd309cfa4c65ef6e9572e
Stephanos Malikotsinas <stephanos@malikotsinas.de>
2019-10-02 10:18:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var button = document.getElementById('hint');
 button.addEventListener('click', function() {
     button.innerText = 'Auf, in eine neue Welt';
     button.onclick = function() {
-        window.location.href = "http://xn--fr-jenny-65a.de/";
+        window.location.href = "farbensterben.de";
     }
 });

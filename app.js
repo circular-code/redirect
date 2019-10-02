4420b21cb2b66428bfb4bb02c66f8f06c8f557cd
RocketScience <stephanos@malikotsinas.de>
2019-10-02 08:11:40 +0200

    init

---
+++ app.js
@@ -0,0 +1,7 @@
+var button = document.getElementById('hint');
+button.addEventListener('click', function() {
+    button.innerText = 'Auf, in eine neue Welt';
+    button.onclick = function() {
+        window.location.href = "http://xn--fr-jenny-65a.de/";
+    }
+});

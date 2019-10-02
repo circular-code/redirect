b0c25aaf716439087400bf16079a7abd2ea38e44
Stephanos Malikotsinas <stephanos@malikotsinas.de>
2019-10-02 17:35:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var button = document.getElementById('hint');
 button.addEventListener('click', function() {
     button.innerText = 'Auf, in eine neue Welt';
     button.onclick = function() {
-        window.location.href = "farbensterben.de";
+        window.location.href = "https://circular-code.github.io/redirect2/";
     }
 });
